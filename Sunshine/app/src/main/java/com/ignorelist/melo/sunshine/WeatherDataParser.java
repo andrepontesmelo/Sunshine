7424d151e1bc5b5f270def266b0ca2c6e4184fad
andrepontes@gmail.com <mara1mar>
2015-07-20 11:56:45 -0300

    Moving parser to WeatherDataParser

--- Sunshine/app/src/main/java/com/ignorelist/melo/sunshine/WeatherDataParser.java
+++ Sunshine/app/src/main/java/com/ignorelist/melo/sunshine/WeatherDataParser.java
@@ -1,8 +1,14 @@
 package com.ignorelist.melo.sunshine;
 
+import android.text.format.Time;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class WeatherDataParser {
 
@@ -18,4 +24,66 @@ public class WeatherDataParser {
         return day.getJSONObject("temp").getDouble("max");
     }
 
+    private String getReadableDateString(long time){
+        SimpleDateFormat shortenedDateFormat = new SimpleDateFormat("EEE MMM dd");
+        return shortenedDateFormat.format(time);
+    }
+
+    private String formatHighLows(double high, double low) {
+        long roundedHigh = Math.round(high);
+        long roundedLow = Math.round(low);
+
+        String highLowStr = roundedHigh + "/" + roundedLow;
+        return highLowStr;
+    }
+
+    ArrayList<String> getWeatherDataArrayListFromJson(String forecastJsonStr, int numDays) throws JSONException {
+        return new ArrayList(Arrays.asList(getWeatherDataFromJson(forecastJsonStr, 7)));
+    }
+
+    public String[] getWeatherDataFromJson(String forecastJsonStr, int numDays)
+            throws JSONException {
+
+        final String OWM_LIST = "list";
+        final String OWM_WEATHER = "weather";
+        final String OWM_TEMPERATURE = "temp";
+        final String OWM_MAX = "max";
+        final String OWM_MIN = "min";
+        final String OWM_DESCRIPTION = "main";
+
+        JSONObject forecastJson = new JSONObject(forecastJsonStr);
+        JSONArray weatherArray = forecastJson.getJSONArray(OWM_LIST);
+
+        Time dayTime = new Time();
+        dayTime.setToNow();
+
+        int julianStartDay = Time.getJulianDay(System.currentTimeMillis(), dayTime.gmtoff);
+
+        dayTime = new Time();
+
+        String[] resultStrs = new String[numDays];
+        for(int i = 0; i < weatherArray.length(); i++) {
+            String day;
+            String description;
+            String highAndLow;
+
+            JSONObject dayForecast = weatherArray.getJSONObject(i);
+
+            long dateTime;
+            dateTime = dayTime.setJulianDay(julianStartDay+i);
+            day = getReadableDateString(dateTime);
+
+            JSONObject weatherObject = dayForecast.getJSONArray(OWM_WEATHER).getJSONObject(0);
+            description = weatherObject.getString(OWM_DESCRIPTION);
+
+            JSONObject temperatureObject = dayForecast.getJSONObject(OWM_TEMPERATURE);
+            double high = temperatureObject.getDouble(OWM_MAX);
+            double low = temperatureObject.getDouble(OWM_MIN);
+
+            highAndLow = formatHighLows(high, low);
+            resultStrs[i] = day + " - " + description + " - " + highAndLow;
+        }
+
+        return resultStrs;
+    }
 }
