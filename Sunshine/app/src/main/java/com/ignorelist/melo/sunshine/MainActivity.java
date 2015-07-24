25a60a0b5bf46ebce42df97860bd3e3d9b4e599e
andrepontes@gmail.com <mara1mar>
2015-07-23 23:09:49 -0300

    Starting setting activity

--- Sunshine/app/src/main/java/com/ignorelist/melo/sunshine/MainActivity.java
+++ Sunshine/app/src/main/java/com/ignorelist/melo/sunshine/MainActivity.java
@@ -1,12 +1,13 @@
 package com.ignorelist.melo.sunshine;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +27,7 @@ public class MainActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
 
